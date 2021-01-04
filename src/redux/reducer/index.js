dff327bf01cc97d95aabc17d63d96efe89ddfe37
JoseOgam <jotienoochieng@gmail.com>
2021-01-04 13:37:46 -0500

    designing root reducers

---
+++ src/redux/reducer/index.js
@@ -0,0 +1,9 @@
+import {combineReducers} from 'redux'
+import authReducer from './auth'
+import messageReducer from './message'
+const rootReducer = combineReducers({
+    authReducer,
+    messageReducer
+})
+
+export default rootReducer
